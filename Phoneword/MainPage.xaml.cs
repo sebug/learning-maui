b094d3cea64d0635535b49922621f8647054ffc0
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2023-12-19 17:36:38 +0100

    Change increment

--- Phoneword/MainPage.xaml.cs
+++ Phoneword/MainPage.xaml.cs
@@ -11,7 +11,7 @@ public partial class MainPage : ContentPage
 
 	private void OnCounterClicked(object sender, EventArgs e)
 	{
-		count++;
+		count += 5;
 
 		if (count == 1)
 			CounterBtn.Text = $"Clicked {count} time";
